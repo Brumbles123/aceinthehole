e40fb1e9ff8c0c67b555b73257caae035934f447
Brumbles123 <52513461+Brumbles123@users.noreply.github.com>
2019-07-30 16:14:21 -0700

    Add files via upload

---
+++ template/includes/nav.inc.html.php
@@ -0,0 +1,10 @@
+<nav class="button">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Event Essentials</a></li>
+        <li><a href="#">Registration</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">FAQs</a></li>
+    </ul>
+</nav>
