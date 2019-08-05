7981bd420c26c1c858b53886235ac2be6104a546
Brumbles123 <52513461+Brumbles123@users.noreply.github.com>
2019-08-05 00:40:14 -0700

    Delete social.inc.html.php

--- template/includes/social.inc.html.php
+++
@@ -1,5 +0,0 @@
-<aside id="social">
-    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcas222cascade%2F&tabs=timeline&width=300&height=300&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="250" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-    <br><br>
-    <a class="twitter-timeline" data-width="300" data-height="300" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-</aside>
