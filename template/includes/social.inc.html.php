f8d805e3390d02943dc9fd1f06f563d34a69fdb7
Brumbles123 <52513461+Brumbles123@users.noreply.github.com>
2019-08-05 00:40:45 -0700

    Add files via upload

---
+++ template/includes/social.inc.html.php
@@ -0,0 +1,5 @@
+<aside id="social">
+    <div class="fb-page" data-href="https://www.facebook.com/cas222cascade/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cas222cascade/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cas222cascade/">CAS 222</a></blockquote></div>
+    <br><br>
+    <a class="twitter-timeline" data-width="300" data-height="300" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</aside>
