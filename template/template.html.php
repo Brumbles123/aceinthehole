a96c2c6b8592f9c11de216c72cefc3620c9ce745
Brumbles123 <52513461+Brumbles123@users.noreply.github.com>
2019-08-05 00:39:29 -0700

    Add files via upload

---
+++ template/template.html.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ace In the Hole Multisport Events Weekend</title>
+    <meta name="description" content="Ace In the Hole Multisport Events offers Events for Every Body">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/helper.css">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+    <body>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0"></script>
+        <div id="page-container">
+            <div id="content-wrap">
+                <?php include '../includes/header.inc.html.php'; ?>
+                <?php include '../includes/nav.inc.html.php'; ?>
+
+
+                <main>
+                    <section id="feature">
+                        <h2>Featured Content Goes Here</h2>
+                        <p>&nbsp; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan lobortis diam. Integer finibus metus sit amet imperdiet eleifend. Nullam metus justo, auctor non sapien sit amet, dictum lobortis odio. Mauris pellentesque eleifend arcu et finibus. Proin pretium nunc et tellus ultrices, vel sollicitudin libero semper. Donec egestas, odio eu laoreet viverra, elit neque ultricies nunc, et sagittis turpis lacus sit amet orci. Suspendisse dapibus odio nulla, a commodo nibh dictum non. Vestibulum eget quam vitae dolor tincidunt aliquam a non tortor. Vestibulum nulla augue, congue et ultricies id, semper a elit. </p>
+                    </section>
+
+                <aside>
+                    <?php include "../includes/weather.inc.html.php"; ?>
+                    <?php include "../includes/social.inc.html.php"; ?>
+                </aside>
+
+                </main>
+        
+            </div>
+            
+                <?php include '../includes/footer.inc.html.php'; ?>
+    
+        </div>
+        
+        <script  src="scripts/jquery-3.3.1.min.js"></script>
+        <script  src="scripts/ace.js"></script>
+
+    </body>
+
+</html>
